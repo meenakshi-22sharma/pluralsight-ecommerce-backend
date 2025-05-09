a8817a98b379285fa2a1fc573436dd7eca4b4759
Meenakshi Sharma <mee2208sharma@gmail.com>
2025-05-10 00:49:27 +0530

    db connection

--- src/main/java/com/pluralsight/ecommerce/EcommerceApplication.java
+++ src/main/java/com/pluralsight/ecommerce/EcommerceApplication.java
@@ -1,10 +1,35 @@
 package com.pluralsight.ecommerce;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication implements CommandLineRunner {
+
+//	@Value("${spring.port}")
+//	private String port;
+
+	@Value("${spring.datasource.url}")
+	private String message;
+
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+//
+//	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println( message);
+//		log.info("Resolved message port: {}", port);
+//		log.info("Resolved message parameter: {}", message);
+
+
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
